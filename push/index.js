e7f7b2356089760727302dc8d665eb85ab3357a3
Temsbaby <116002537+Temsbaby@users.noreply.github.com>
2022-11-15 22:33:58 +0100

    Add files via upload

---
+++ push/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const ejs = require("ejs")
+
+const server = express()
+
+server.use(express.static ("public"));
+
+server.set("view engine","ejs")
+
+
+
+server.listen(8000, () =>{
+    console.log("Server dey listen......")
+})
+
+
+
+
+
+server.get("/", (req, res) =>{
+    const newBio = [
+        { name:"Asimiyu Temitope",  score:100,   state:"Osun State",  nationality:"Nigerian"},
+        {name:"Dumebi Chinaza", score:50, state:"Osun state", nationality:"Nigerian"},
+        {name:"Kwesi Arthur", score:90, state:'Accra', nationality:'Ghana'}
+        
+    ]
+
+    
+       
+        
+   
+    
+    res.render("index", {newBio})
+   
+    
+    // persons.map((person)=>{
+        
+    //     console.log(Object.keys(person).toString())
+        
+        
+    // })
+})
